8b4fcc220ff8f513743d3080339142b6bc9cb427
Linda Lacsivy <lindalacsivy@outlook.de>
2025-05-28 00:02:56 +0200

    init.sql done.

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+set -e
 mkdir -p /var/lib/mysql /var/run/mysqld
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+
+# Run the SQL script
+exec mariadbd --init-file=/etc/mysql/init.sql --user=mysql
 
